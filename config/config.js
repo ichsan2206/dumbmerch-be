cc6d175704edca47ecf227dc28365097f243ea7e
ichsan2206 <jamiludin.ihsan3@gmail.com>
2022-09-08 00:15:32 +0700

    update

---
+++ config/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  development: {
+    username: "root",
+    password: "gwjDvPCDtK2NdhcSYpnO",
+    port: 5889,
+    database: "railway",
+    host: "containers-us-west-75.railway.app",
+    dialect: "mysql"
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: { // https://github.com/sequelize/sequelize/issues/12083
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
